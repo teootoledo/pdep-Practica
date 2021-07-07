be831022b40e6a5fbf0507ba2b1b16ee9f2e344d
teootoledo <teootoledo@gmail.com>
2021-07-07 20:13:32 -0300

    Prolog

---
+++ Logico/peliculas.pl
@@ -0,0 +1,14 @@
+% BASE DE CONOCIMIENTOS
+
+actuo("Once upon a time in Hollywood", "Leo Di Caprio").
+actuo("Once upon a time in Hollywood", "Brad Pitt").
+actuo("Once upon a time in Hollywood", "Margot Robbie").
+actuo("Wolf of Wall Street", "Leo Di Caprio").
+actuo("Wolf of Wall Street", "Margot Robbie").
+actuo("Wolf of Wall Street", "Jonah Hill").
+actuo("Good Fellas", "Lorraine Bracco").
+actuo("Good Fellas", "Ray Liotta").
+actuo("Good Fellas", "Robert De Niro").
+actuo("Good Fellas", "Joe Pesci").
+actuo("Catch me if you can", "Leo Di Caprio").
+actuo("Catch me if you can", "Tom Hanks").
